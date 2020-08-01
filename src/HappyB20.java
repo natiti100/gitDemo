a894d9e69f66eb00753eeb28491f0fe014638611
natiti100 <natalia.94@live.com>
2020-08-01 12:07:03 -0400

    HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,7 +3,8 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
 
-        System.out.println("Cybertek");
+        System.out.println("Cybertek is amazing");
+
     }
 
 }
